cc9264e5b42c1ad6ce53d442ed2b587cc0179523
ozlemertosun <U2407193@rts-365.dk>
2021-09-10 10:49:49 +0200

    add solution to assignment

--- js/app.js
+++ js/app.js
@@ -1,37 +1,67 @@
 
+//øvelse 1:
 const jsonObjects = 
 { "classMates" : [ 
     {
     "firstname" : "Pamela", 
     "lastname" : "Anderson", 
     "age" : 54, 
-    "haircolor" : "Blond"
+    "haircolor" : "Blond", 
+    "shoesize": 39
     }, 
 
     {
     "firstname" : "David", 
     "lastname" : "Hasselhof", 
     "age" : 69, 
-    "haircolor" : "Brown"
+    "haircolor" : "Brown", 
+    "shoesize": 44
     }, 
 
     {
     "firstname" : "Jeremy", 
     "lastname" : "Jackson", 
     "age" : 40, 
-    "haircolor" : "Brown"
+    "haircolor" : "Brown", 
+    "shoesize" : 43
     }, 
 
     {
     "firstname" : "Yasmine", 
     "lastname" : "Bleeth", 
     "age" : 53, 
-    "haircolor" : "Brown"
+    "haircolor" : "Brown", 
+    "shoesize" : 38
     }
 ]
 }
-jsonObjects.classMates.forEach(e => console.log(e.firstname));
 
-jsonObjects.classMates.forEach(e => console.log(e.firstname,e.lastname));
+//øvelse 2:
+//jsonObjects.classMates.forEach(e => console.log(e.firstname));
+
+//øvelse 3:
+//jsonObjects.classMates.forEach(e => console.log(e.firstname,e.lastname));
+
+/* jsonObjects.classMates.forEach(e => {
+console.log(`${e.firstname} ${e.lastname}`);
+}); */
+
+
+//øvelse 4:
+/* let ul = document.querySelector("#list");
+
+jsonObjects.classMates.forEach(e => {
+    let listitem = document.createElement("li");
+    listitem.textContent = `${e.firstname} ${e.lastname} ${e.age} ${e.haircolor} ${e.shoesize}`;
+    ul.appendChild(listitem);
+    listitem.style.backgroundColor = "rgb(120,120,120)"
+});  */
+
+//øvelse 5: 
+//se fil: team.json
+
+
+
+
 
 
